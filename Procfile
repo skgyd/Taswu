c3d2cf94285684943ab280036dbab2562fa28caa
Your Name <you@example.com>
2019-07-30 21:41:09 +0900

    first commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn taswuteam.wsgi --log-file -
--- taswuproject/settings.py
+++ taswuproject/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'pzju16vxdgyt(6j-7y&#arf#9zsr-mr+b9msd!(j_q7k&$8oc$'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','pzju16vxdgyt(6j-7y&#arf#9zsr-mr+b9msd!(j_q7k&$8oc$')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = []
 
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -121,3 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
